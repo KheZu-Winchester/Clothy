4a58ad3c94cbf1b44d7d51cc4bfe682c51d507b5
sme <yoopajdoto@gmail.com>
2023-03-28 17:04:34 +0630

    ui for clothy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
+import { TrendComponent } from './trend/trend.component';
+import { DiscountComponent } from './discount/discount.component';
+import { ProductsComponent } from './products/products.component';
+import { ExpressComponent } from './express/express.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroComponent,
+    TrendComponent,
+    DiscountComponent,
+    ProductsComponent,
+    ExpressComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
